3d0170db75c1f73f5169c6855c1d4b9818a9caf6
Eugene Dvoretsky <radioxoma@gmail.com>
2019-04-28 20:41:32 +0300

    Correct spaces handling

--- extractors/ffmpeg-extractsubs.sh
+++ extractors/ffmpeg-extractsubs.sh
@@ -1,50 +1,50 @@
 #!/usr/bin/env bash
 
 # Automatically extract all subtitles from video file
-# and put them in a working directory
+# and put them in a current working directory
 
-if [[ $# -eq 0 ]] ; then
-    echo "Video file name required"
+E_NOARGS=65
+
+if [[ $# -ne 1 ]] ; then
+    echo "Usage: '`basename $0` video.mkv'"
+    exit $E_NOARGS
+fi
+
+# case "$1" in
+#     --help) echo "Extract subtitles from given video and put it in a working directory" & exit 0;;
+#     # *) echo 'you gave something else' ;;
+# esac
+
+if [ ! -f "$1" ] ; then
+    echo "File not found"
     exit 0
 fi
 
-case "$1" in
-    --help) echo "Extract subtitles from given video and put it in a working directory" & exit 0;;
-    # *) echo 'you gave something else' ;;
-esac
-
-filepath="${1}"
 filename=$(basename -- "${1}")
 basename="${filename%.*}"
 
-extract_subs() {
-    # Generate ffmpeg parameters for subtitle extraction
-    streams=(`ffprobe -loglevel error -select_streams s \
-        -show_entries stream=index:stream_tags=language \
-        -of csv=p=0 -i "${1}"`)
+# Generate ffmpeg parameters for subtitle extraction
+streams=(`ffprobe -loglevel error -select_streams s \
+    -show_entries stream=index:stream_tags=language \
+    -of csv=p=0 -i "${1}"`)
 
-    # if [[ ${streams[@]} -eq 0 ]] ; then
-    #     echo "No subtitles?"
-    #     exit 0
-    # fi
+# if [[ ${streams[@]} -eq 0 ]] ; then
+#     echo "No subtitles?"
+#     exit 0
+# fi
 
-    local args=("-i ${filename} -c copy")
+args=()
 
-    for stream in "${streams[@]}"
-    do
-        IFS=','
-        read -ra i <<< "${stream//[$'\t\r\n']}"
-        unset IFS
-        str=${i[0]}
-        lng=${i[1]}
-        # Stream numbers always present, so we use it at first position
-        # in filename for convenient sorting in file manager
-        args+=("-map 0:${str} ${basename}_${str}-${lng}.srt")
-    done
-    echo ${args[@]}
-}
+for stream in "${streams[@]}"
+do
+    IFS=','
+    read -ra i <<< "${stream//[$'\t\r\n']}"
+    # unset IFS
+    str=${i[0]}
+    lng=${i[1]}
+    # Stream numbers always present, so we use it at first position
+    # in filename for convenient sorting in file manager
+    args+=(-map 0:${str} ${basename}-${str}-${lng}.srt)
+done
 
-ffprobe -loglevel error -select_streams s \
-        -show_entries stream=index:stream_tags=language \
-        -of csv=p=0 -i ${filepath}
-ffmpeg -loglevel error `extract_subs $filepath`
+ffmpeg -loglevel error -stats -i "$1" -c copy ${args[@]}
