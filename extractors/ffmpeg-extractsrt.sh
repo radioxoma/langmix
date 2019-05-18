d785d7d96f93a87c5ecfd732b35e231e2cf26296
Eugene Dvoretsky <radioxoma@gmail.com>
2019-05-18 13:53:38 +0300

    Word wrap and conditional

--- extractors/ffmpeg-extractsrt.sh
+++ extractors/ffmpeg-extractsrt.sh
@@ -10,7 +10,7 @@ convert to SRT and put them in the specified folder. Usage:
 where:
     -h  show this help text"
 
-if [[ $# -eq 0 ]] ; then
+if [[ $# -ne 2 ]] ; then
     echo "$usage"
     exit $E_NOARGS
 fi
@@ -23,16 +23,19 @@ fi
 filename=$(basename -- "${1}")
 basename="${filename%.*}"
 
-echo 'Streams found:'
-ffprobe -i "$1" -loglevel error -show_entries stream=index:stream_tags=language:stream_tags=title:stream=codec_type:stream=codec_long_name -of csv=p=0
-echo
+echo "Streams found:"
+ffprobe -i "$1" -loglevel error \
+-show_entries stream=index:stream_tags=language:stream_tags=title:\
+stream=codec_type:stream=codec_long_name -of csv=p=0
 
 streams=(`ffprobe -i "$1" -loglevel error -select_streams s \
     -show_entries stream=index:stream_tags=language -of csv=p=0`)
 
 if [ ${#streams[@]} -eq 0 ]; then
-    echo "No subtitles?"
+    echo "No subtitles in file?"
     exit 0
+else
+    echo "Extracting subtitles as SRT..."
 fi
 
 # Generate ffmpeg parameters for subtitle extraction
