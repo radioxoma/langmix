dec9a93f8a9d9a14fe4c84a5213b3fa7eec38132
Eugene Dvoretsky <radioxoma@gmail.com>
2019-09-19 19:41:50 +0300

    Idiomatic bash

--- extractors/ffmpeg-extractsrt.sh
+++ extractors/ffmpeg-extractsrt.sh
@@ -28,8 +28,8 @@ ffprobe -i "$1" -loglevel error \
 -show_entries stream=index:stream_tags=language:stream_tags=title:\
 stream=codec_type:stream=codec_long_name -of csv=p=0
 
-streams=$(ffprobe -i "$1" -loglevel error -select_streams s \
-    -show_entries stream=index:stream_tags=language -of csv=p=0)
+streams=( $(ffprobe -i "$1" -loglevel error -select_streams s \
+    -show_entries stream=index:stream_tags=language -of csv=p=0) )
 
 if [ ${#streams[@]} -eq 0 ]; then
     echo "No subtitles in file?"
@@ -40,12 +40,12 @@ fi
 
 # Generate ffmpeg parameters for subtitle extraction
 args=()
-for stream in ${streams[@]}
+for stream in "${streams[@]}"
 do
-    IFS=',' read -ra i <<< "${stream//[$'\t\r\n']}"
+    IFS=',' read -ra i <<< "${stream//[$'\r\n']}"
     str=${i[0]}
     lng=${i[1]}
-    # Stream numbers always present, so we use it at first position
+    # Stream numbers are always present, so we use them at first position
     # in filename for convenient sorting in file manager
     args+=(-map 0:"${str}" "${2}/${basename}-${str}-${lng}.srt")
 done
